7621714001cef65d2237daf7c6da8cde8bf7a071
Alejandro Penagos <alejandropenagos1998@gmail.com>
2023-02-16 01:01:58 -0500

    Create main list items for recycler views for Home Fragment

---
+++ app/src/main/java/com/abstractcoder/baudoapp/recyclers/ImagePostAdapter.kt
@@ -0,0 +1,39 @@
+package com.abstractcoder.baudoapp.recyclers
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.abstractcoder.baudoapp.R
+import com.google.android.material.imageview.ShapeableImageView
+
+class ImagePostAdapter(private val imagePostList: ArrayList<ImagePostMain>) : RecyclerView.Adapter<ImagePostAdapter.ImagePostHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ImagePostHolder {
+        val itemView = LayoutInflater.from(parent.context).inflate(R.layout.image_list_item,
+        parent, false)
+        return ImagePostHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: ImagePostHolder, position: Int) {
+        val currentItem = imagePostList[position]
+        //holder.imageThumbnail.setImageURI(currentItem.thumbnail)
+        holder.imageThumbnail.setImageResource(currentItem.thumbnail)
+        holder.imageAuthor.text = currentItem.author
+        holder.imageDescription.text = currentItem.description
+    }
+
+    override fun getItemCount(): Int {
+        return imagePostList.size
+    }
+
+    class ImagePostHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+        val imageThumbnail: ShapeableImageView = itemView.findViewById(R.id.image_list_item_media)
+        val imageAuthor: TextView = itemView.findViewById(R.id.image_list_item_author)
+        val imageDescription: TextView = itemView.findViewById(R.id.image_list_item_description)
+
+    }
+
+}
---
+++ app/src/main/java/com/abstractcoder/baudoapp/recyclers/ImagePostMain.kt
@@ -0,0 +1,5 @@
+package com.abstractcoder.baudoapp.recyclers
+
+import android.net.Uri
+
+data class ImagePostMain(var thumbnail: Int, var author: String, var description: String)
