ea7efc35c93c41c45248f53a676b332888999e52
Alejandro Penagos <alejandropenagos1998@gmail.com>
2023-02-16 00:59:28 -0500

    create settings popup fragment

---
+++ app/src/main/java/com/abstractcoder/baudoapp/fragments/SettingsPopUpFragment.kt
@@ -0,0 +1,21 @@
+package com.abstractcoder.baudoapp.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.abstractcoder.baudoapp.R
+
+class SettingsPopUpFragment : DialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_settings_pop_up, container, false)
+    }
+
+}
