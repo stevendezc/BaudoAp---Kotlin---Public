85d7d93a31e997b704c8ed02c6c6cee2ae85f835
Alejandro Penagos <alejandropenagos1998@gmail.com>
2023-03-25 04:00:54 -0500

    Create CommunityData data class

---
+++ app/src/main/java/com/abstractcoder/baudoapp/CommunityData.kt
@@ -0,0 +1,14 @@
+package com.abstractcoder.baudoapp
+
+data class CommunityData(
+    val id: String,
+    val category: String,
+    val description: String,
+    val facebook: String,
+    val instagram: String,
+    val lastname: String,
+    val name: String,
+    val thumbnail: String,
+    val twitter: String,
+    val whatsapp: String
+)
