946b9ca20e802fe6b39984cbe7e740b870993bab
Alejandro Penagos <alejandropenagos1998@gmail.com>
2023-04-03 16:22:23 -0500

    Update resized image display logic

--- app/src/main/java/com/abstractcoder/baudoapp/resizedImageActivity.kt
+++ app/src/main/java/com/abstractcoder/baudoapp/resizedImageActivity.kt
@@ -1,10 +1,16 @@
 package com.abstractcoder.baudoapp
 
+import android.graphics.drawable.Drawable
 import android.net.Uri
 import android.os.Bundle
+import android.view.ViewTreeObserver
 import androidx.appcompat.app.AppCompatActivity
 import com.abstractcoder.baudoapp.databinding.ActivityResizedImageBinding
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 
 class resizedImageActivity : AppCompatActivity() {
 
@@ -16,8 +22,6 @@ class resizedImageActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         val imageUrl = intent.getParcelableExtra<Uri>("image")
-        println("ImageUrl")
-        println(imageUrl)
 
         setup(imageUrl!!)
     }
@@ -29,8 +33,40 @@ class resizedImageActivity : AppCompatActivity() {
 
         Glide.with(binding.resizedImage)
             .load(imageUrl)
-            .centerCrop()
+            .listener(object : RequestListener<Drawable> {
+                override fun onLoadFailed(
+                    e: GlideException?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    // handle failed image loading here
+                    return false
+                }
+                override fun onResourceReady(
+                    resource: Drawable?,
+                    model: Any?,
+                    target: Target<Drawable>?,
+                    dataSource: DataSource?,
+                    isFirstResource: Boolean
+                ): Boolean {
+                    val imageWidth = resource?.intrinsicWidth
+                    val scrollViewWidth = binding.imageScrollView.width
+                    val desiredXPosition = (imageWidth!! / 2) - (scrollViewWidth / 2)
+                    binding.resizedImage.viewTreeObserver.addOnGlobalLayoutListener(object :
+                        ViewTreeObserver.OnGlobalLayoutListener {
+                        override fun onGlobalLayout() {
+                            // Remove the listener to avoid multiple calls
+                            binding.resizedImage.viewTreeObserver.removeOnGlobalLayoutListener(this)
+                            // Get the x-coordinate of the middle of the image view
+                            val middleX = desiredXPosition
+                            // Scroll to the middle of the image view
+                            binding.imageScrollView.scrollTo(middleX.toInt(), 0)
+                        }
+                    })
+                    return false
+                }
+            })
             .into(binding.resizedImage)
-
     }
 }
